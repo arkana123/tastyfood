1dc833c0ed7c6e3da71091da63f9560143187db7
MUHAMMAD ARKANA <88721820+arkana123@users.noreply.github.com>
2023-07-05 13:07:20 +0700

    dashboard admin

---
+++ lib/admin/dashboardAdmin.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class dashboardAdmin extends StatefulWidget {
+  const dashboardAdmin({super.key});
+
+  @override
+  State<dashboardAdmin> createState() => _dashboardAdminState();
+}
+
+class _dashboardAdminState extends State<dashboardAdmin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
